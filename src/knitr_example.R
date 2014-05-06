ad5011226a082f72c5b08c88ec82d94deae5cf5c
Ryan Elmore <Ryan.Elmore@nrel.gov>
2014-05-05 22:15:22 -0600

    woo hoo, bootstrap is working

--- src/knitr_example.R
+++ src/knitr_example.R
@@ -5,8 +5,9 @@
 library(knitr)
 library(rmarkdown)
 library(ggplot2)
-library(XLConnect)
 library(knitrBootstrap)
 
-render("knitr_example.Rmd", output_file = "index-tmp.html")
-bootstrap_HTML("index-tmp.html", output = "../index.html")
+render("knitr_example.Rmd", 
+       output_file = "../index.html",
+       'knitrBootstrap::bootstrap_document')
+
